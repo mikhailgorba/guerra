30c6df15344fa63794fa5b518f2758ad00795911
Nome Sobrenome <aleson.morais@coopersystem.com>
2018-05-02 11:12:55 -0300

    iniciando
    upando todo o projeto

---
+++ materialize/js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+ $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+        
